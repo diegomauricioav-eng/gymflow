1c8dba39bb15da3ee891ab044df585266137eb3a
Hansel <hanseldalicr@ufps.edu.co>
2026-06-22 17:23:35 -0500

    feat: Implementacion de gymflow-api

--- gymflow-api/src/main/java/co/ufps/gymflow/api/config/JaxRsApplication.java
+++ gymflow-api/src/main/java/co/ufps/gymflow/api/config/JaxRsApplication.java
@@ -3,7 +3,7 @@ package co.ufps.gymflow.api.config;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api") // La URL base será: http://localhost:8080/gymflow/api
+@ApplicationPath("/api") // La URL base será: http://localhost:4848/gymflow/api
 public class JaxRsApplication extends Application {
     // Se deja vacía. Jakarta EE se encarga de escanear los controladores.
 }
